c9c18a229066afa780ef12d9b111623709189a0c
weno42 <obroberts@gmail.com>
2015-07-21 17:08:09 -0400

    new pig images

--- bkg.js
+++ bkg.js
@@ -2,25 +2,21 @@ window.onload = function() {
 	var width = window.innerWidth;
 	var bkg = document.getElementById('bkg');
 	var img = new Image();
-	img.src = 'imgs/eatingmachine00.png';
 	bkg.appendChild(img);
-	var imgs = [ 
-	"eatingmachine00.png",
-	"eatingmachine01.png",
-	"eatingmachine02.png",
-	"eatingmachine03.png",
-	"eatingmachine04.png",
-	"eatingmachine05.png",
-	"eatingmachine06.png",
-	"eatingmachine07.png",
-	"eatingmachine08.png",
-	"eatingmachine09.png"
-	];
+	var imgs = [];
+	for (var i = 3; i < 143; i++) {
+		if (i < 10) imgs.push("imgs/piggy/piggy"+0+0+0+i+".jpg");
+		else if (i < 100) imgs.push("imgs/piggy/piggy"+0+0+i+".jpg");
+		else imgs.push("imgs/piggy/piggy"+0+i+".jpg");
+	}
 	var columnWidth = width / imgs.length;
+	console.log(imgs.length);
+	console.log(Math.floor(Math.random()*imgs.length));
+	img.src = imgs[Math.floor(Math.random()*imgs.length)];
 
 	document.onmousemove = function(ev) {
 		var xpos = ev.pageX;
 		var num = Math.floor(xpos/columnWidth);
-		img.src = 'imgs/' + imgs[num];
+		img.src = imgs[num];
 	}
 };
