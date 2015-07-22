ccc159a31ac55be3cb583cc64990077aad372b58
weno42 <obroberts@gmail.com>
2015-07-22 18:07:48 -0400

    background preload images

--- bkg.js
+++ bkg.js
@@ -1,22 +1,21 @@
 window.onload = function() {
 	var width = window.innerWidth;
 	var bkg = document.getElementById('bkg');
-	var img = new Image();
-	bkg.appendChild(img);
 	var imgs = [];
 	for (var i = 3; i < 143; i++) {
-		if (i < 10) imgs.push("imgs/piggy/piggy"+0+0+0+i+".jpg");
-		else if (i < 100) imgs.push("imgs/piggy/piggy"+0+0+i+".jpg");
-		else imgs.push("imgs/piggy/piggy"+0+i+".jpg");
+		var img = new Image();
+		if (i < 10) 		img.src = "imgs/piggy/piggy"+0+0+0+i+".jpg";
+		else if (i < 100) 	img.src = "imgs/piggy/piggy"+0+0+i+".jpg";
+		else 				img.src = "imgs/piggy/piggy"+0+i+".jpg";
+		imgs.push(img);
 	}
 	var columnWidth = width / imgs.length;
-	console.log(imgs.length);
-	console.log(Math.floor(Math.random()*imgs.length));
-	img.src = imgs[Math.floor(Math.random()*imgs.length)];
+	bkg.appendChild(imgs[Math.floor(Math.random()*imgs.length)]);
 
 	document.onmousemove = function(ev) {
 		var xpos = ev.pageX;
 		var num = Math.floor(xpos/columnWidth);
-		img.src = imgs[num];
+		bkg.removeChild(bkg.firstChild);
+		bkg.appendChild(imgs[num]);
 	}
 };
