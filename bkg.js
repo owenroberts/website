5c30cc38e5eb2c33831f2c5d83f9e431e29480da
Owen Roberts <obr208@nyu.edu>
2016-03-14 19:16:03 -0400

    separates pages and stuff

--- bkg.js
+++ bkg.js
@@ -1,4 +1,4 @@
-window.onload = function() {
+window.addEventListener('load', function() {
 
 	// ** check for mobile ** http://www.abeautifulsite.net/detecting-mobile-devices-with-javascript/
 	var isMobile = {
@@ -30,9 +30,9 @@ window.onload = function() {
 		numImages = 143;
 	}
 	
-
-	var width = window.innerWidth;
 	var bkg = document.getElementById('bkg');
+
+	var width = bkg.offsetWidth;
 	var imgs = [];
 	for (var i = 3; i < numImages; i++) {
 		var img = new Image();
@@ -41,18 +41,25 @@ window.onload = function() {
 		else 				img.src = "imgs/piggy/piggy"+0+i+".jpg";
 		imgs.push(img);
 		bkg.appendChild(img);
+		bkg.removeChild(img);
 	}
-	var columnWidth = width / imgs.length;
+	var columnWidth;
+	var setSize = function() {
+		columnWidth = window.innerWidth / imgs.length;
+	}
+	setSize();
 	bkg.appendChild(imgs[Math.floor(Math.random()*imgs.length)]);
 
 	if (!isMobile.any()) {
-		var b = document.querySelector('body');
-		b.onmousemove = function(ev) {
+		document.onmousemove = function(ev) {
 			var xpos = ev.pageX;
 			var num = Math.floor(xpos/columnWidth);
 			bkg.removeChild(bkg.firstChild);
 			bkg.appendChild(imgs[num]);
 		}
+		window.onresize = function(ev) {
+			setSize();
+		}
 	}
 
-};
+});
---
+++ cool.js
@@ -0,0 +1,38 @@
+window.addEventListener('load', function() {
+	String.prototype.replaceAt=function(index, character) {
+    	return this.substr(0, index) + character + this.substr(index+character.length);
+	}
+
+	var title = "😬😬 😬😬"
+	var alttitle = "😁😁 😁😁";
+	var time = 500;
+	var timer = Date.now();
+	
+	var b = document.querySelector("#butt");
+	var t = b.innerHTML;
+	
+	var letterMove = function() {
+		var n = Math.floor(Math.random() * t.length);
+		var temp = t[n];
+		var r = n;
+
+		r +=  Math.round(Math.random()) ? -1 : 1;
+		if (r > 0 && r < t.length) {
+			var othertemp = t[r];
+			t = t.replaceAt(n, othertemp);
+			t = t.replaceAt(r, temp);
+			b.innerText = t;
+		}
+	}
+	
+	var loop = function() {
+		requestAnimationFrame(loop);
+		if (Date.now() > timer + time) {
+			timer = Date.now();
+			if (document.title == title) document.title = alttitle;
+			else document.title = title;
+			letterMove();
+		}
+	};
+	requestAnimationFrame(loop);
+});
